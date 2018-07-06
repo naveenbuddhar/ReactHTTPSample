bc906866326235d1885f46d9ea012577c9a19ad5
Kumar <Naveen.Kumar@acer.com>
2018-07-06 15:52:45 +0400

    first commit

---
+++ src/containers/Blog/Blog.js
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+//import axios from "axios";
+import "./Blog.css";
+import Posts from "./Posts/Posts";
+import { Route, NavLink } from "react-router-dom";
+import NewPost from "./NewPost/NewPost.js";
+//import { Link } from "react-router-relative-link";
+
+class Blog extends Component {
+  render() {
+    return (
+      <div className="Blog">
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <NavLink
+                  to="/"
+                  //assign the classname from css
+                  activeClassName="my-active"
+                  //Inline Styling
+                  activeStyle={{
+                    color: "#fa923f",
+                    textDecoration: "underline"
+                  }}
+                  exact
+                >
+                  Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to={{
+                    //Relative path using react-router-relative-link
+                    pathname: "/new-post",
+                    hash: "#submit",
+                    search: "?quick-submit=true"
+                  }}
+                >
+                  New Post
+                </NavLink>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        {/*<Route path="/" exact render={() => <h1> home </h1>} />*/}
+        <Route path="/" exact component={Posts} />
+        <Route path="/new-post" component={NewPost} />
+      </div>
+    );
+  }
+}
+
+export default Blog;
---
+++ src/containers/Blog/Posts/Posts.js
@@ -0,0 +1,57 @@
+import React, { Component } from "react";
+import axios from "../../../axios";
+import Post from "../../../components/Post/Post";
+import "./Posts.css";
+
+class Posts extends Component {
+  state = {
+    posts: [],
+    selectedPostId: null,
+    errorOccured: false
+  };
+  componentDidMount() {
+    console.log(this.props);
+    axios
+      .get("posts")
+      .then(response => {
+        const posts = response.data.slice(0, 4);
+        const updatedPosts = posts.map(post => {
+          return {
+            ...post,
+            author: "Naveen"
+          };
+        });
+
+        this.setState({ posts: updatedPosts });
+        //console.log(response);
+      })
+      .catch(err => {
+        console.log(err);
+        //this.setState({ errorOccured: true });
+      });
+  }
+  selectedPostHandler = id => {
+    this.setState({ selectedPostId: id });
+    console.log("[SelectedPostID]" + id + this.selectedPostId);
+  };
+  render() {
+    let posts = (
+      <p style={{ textAlign: "center" }}> Something Went Wrong!!! </p>
+    );
+    if (!this.state.errorOccured) {
+      posts = this.state.posts.map(post => {
+        return (
+          <Post
+            key={post.id}
+            title={post.title}
+            author={post.author}
+            clicked={() => this.selectedPostHandler(post.id)}
+          />
+        );
+      });
+    }
+    return <section className="Posts">{posts}</section>;
+  }
+}
+
+export default Posts;
